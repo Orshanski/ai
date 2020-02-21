b710a917f5944f3abdf3708905a23586d4964b4e
aorshanskiy <alexey.orshanski@gmail.com>
2020-02-21 17:30:01 +0300

    Добавлен класс settings.py - настройки. Добавлен класс ship.py - корабль и его изображение в папку images.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
-
 import pygame
+from settings import Settings
+from ship import Ship
 
 
 class AlienInvasion:
@@ -9,9 +10,12 @@ class AlienInvasion:
     def __init__(self):
         """Инициализирует игру и создает игровые ресурсы"""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((self.settings.screen_wight, self.settings.screen_height))
         pygame.display.set_caption('Alien Invasion')
+        self.ship = Ship(self)
+
 
     def run_game(self):
         """Запуск основного цикла игры"""
@@ -20,6 +24,9 @@ class AlienInvasion:
             for invent in pygame.event.get():
                 if invent.type == pygame.QUIT:
                     sys.exit()
+
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             # Отображение последнего прорисованного экрана
             pygame.display.flip()
---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    """ Класс для хранения настроект игры"""
+
+    def __init__(self):
+        """ Инициализирует настройки игры"""
+        # Параметры экрана
+        self.screen_wight = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
